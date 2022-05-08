40e3a3236ccc5f8bf8c3f461bd192ca525920e55
juan <juankmiloh@hotmail.com>
2022-05-07 22:19:15 -0500

    add frontcontroller to the project

--- Backend/src/controller/__init__.py
+++ Backend/src/controller/__init__.py
@@ -5,6 +5,7 @@ controller = Blueprint('controller', __name__, url_prefix='/')
 # src.controller
 
 from . import \
+front_controller, \
 anios_controller, \
 causas_controller, \
 empresas_controller, \
---
+++ Backend/src/controller/front_controller.py
@@ -0,0 +1,58 @@
+from flask import render_template, request
+from ..controller import controller
+from ..util.constants import API_ROOT_PATH 
+
+
+@controller.route('', methods=['GET'], defaults={'path': ''})
+def home(path):
+    script_root  = request.script_root
+    
+    if script_root :
+        script_root += "/"
+
+    return render_template("dist/index.html", url_base=script_root + "interrupciones/front/dist")
+
+@controller.route('/map-page', methods=['GET'], defaults={'path': ''})
+def home_api(path):
+    script_root  = request.script_root
+    
+    if script_root :
+        script_root += "/"
+
+    return render_template("dist/index.html", url_base=script_root + "interrupciones/front/dist" )
+
+@controller.route('front/dist/', methods=['GET'], defaults={'path': ''})
+def home_apiv1(path):
+    script_root  = request.script_root
+    
+    if script_root :
+        script_root += "/"
+
+    return render_template("dist/index.html", url_base=script_root + "interrupciones/front/dist" )
+
+@controller.route('interrupciones/front/dist/', methods=['GET'], defaults={'path': ''})
+def home_apiv2(path):
+    script_root  = request.script_root
+    
+    if script_root :
+        script_root += "/"
+
+    return render_template("dist/index.html", url_base=script_root + "interrupciones/front/dist" )
+
+@controller.route('interrupciones/', methods=['GET'], defaults={'path': ''})
+def home_apiv3(path):
+    script_root  = request.script_root
+    
+    if script_root :
+        script_root += "/"
+
+    return render_template("dist/index.html", url_base=script_root + "interrupciones/front/dist" )
+
+@controller.route('front/dist/map-page', methods=['GET'], defaults={'path': ''})
+def home_apiv4(path):
+    script_root  = request.script_root
+    
+    if script_root :
+        script_root += "/"
+
+    return render_template("dist/index.html", url_base=script_root + "interrupciones/front/dist" )
