cfe871d9f86cd59cf08672bfecd5f7e62308d2ca
juan <juankmiloh@hotmail.com>
2022-05-16 12:36:44 -0500

    Se instala SSL en el servidor de la SSPD

--- Backend/server.sh
+++ Backend/server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # echo levantar servidor python
 source venv/bin/activate
-flask run -h 0.0.0.0 -p 5058
-# flask run --cert=server.crt --key=server.key -h 0.0.0.0 -p 5058
+# flask run -h 0.0.0.0 -p 5058
+flask run --cert=server.crt --key=server.key -h 0.0.0.0 -p 5058
