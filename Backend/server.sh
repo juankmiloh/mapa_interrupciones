a7254fb769e3cd45e829749c6d2e7d41c511f289
juan <juankmiloh@hotmail.com>
2022-06-03 23:34:34 -0500

    Change https por http

--- Backend/server.sh
+++ Backend/server.sh
@@ -2,4 +2,5 @@
 # echo levantar servidor python
 source venv/bin/activate
 # flask run -h 0.0.0.0 -p 5058
-flask run --cert=server.crt --key=server.key -h 0.0.0.0 -p 5058
+# flask run --cert=server.crt --key=server.key -h 0.0.0.0 -p 5058
+flask run -h 0.0.0.0 -p 5058
